34d06c1dc8f945864ee22199e3c9c9ddc52d422d
edziograczer <71726555+edziograczer@users.noreply.github.com>
2020-11-24 11:34:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt install zip unzip rar unrar
 pacman -S zip unzip rar unrar
 dnf install zip unzip rar unrar
-cp arche /usr/bin
-cp arche.py /usr/bin
+sudo cp arche /usr/bin && cp arche.py /usr/bin
 echo "completed!"
